a3cacfe63cf84c8df677543209177578e1c67a54
xanmankey <86136010+xanmankey@users.noreply.github.com>
2025-03-04 09:19:37 -0500

    reverting socket stuff

--- src/utils.tsx
+++ src/utils.tsx
@@ -8,5 +8,5 @@ export const socket = io("wss://xanmankey.vulcan.moe", {
   reconnectionDelay: 1000, // Reconnect after 1 second
   reconnectionDelayMax: 5000, // Maximum delay of 5 seconds
   timeout: 20000, // Connection timeout of 20 seconds
-  autoConnect: false, // disable autoConnect on import
+  // autoConnect: false, // disable autoConnect on import
 });
