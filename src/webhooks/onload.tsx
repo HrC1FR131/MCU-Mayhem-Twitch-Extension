c7da15c91db8cd5730b47875d3155045e8b33641
xanmankey <86136010+xanmankey@users.noreply.github.com>
2025-03-05 10:00:08 -0500

    added onAuthorized hook

--- src/webhooks/onload.tsx
+++ src/webhooks/onload.tsx
@@ -1,11 +1,11 @@
 // import { BACKEND } from "../utils";
 // // import dotenv from "dotenv";
 // // dotenv.config({ path: "../../.env" });
-// declare global {
-//   interface Window {
-//     Twitch: any;
-//   }
-// }
+declare global {
+  interface Window {
+    Twitch: any;
+  }
+}
 
 // // export let username: string;
 
@@ -19,39 +19,41 @@
 // //   userId: string;
 // // }
 
-// // window.Twitch.ext.onAuthorized(function (auth: any) {
-// //   // Turns out you can't get the user's username from the Twitch API using a user JWT token
-// //   // You need an access token, but I don't want to make the user deal with oauth
-// //   // So I'll just allow them to type their names in
-// //   // console.log(auth);
-// //   // const oauthToken = auth.helixToken;
-// //   // console.log(oauthToken);
-// //   // const clientId = auth.clientId; // Replace with your Twitch client ID
-// //   // const userId = auth.userId;
-// //   // console.log(userId);
-// //   // fetch("https://api.twitch.tv/helix/users", {
-// //   //   method: "GET",
-// //   //   headers: {
-// //   //     "client-id": clientId!,
-// //   //     Authorization: `Extension ${oauthToken}`,
-// //   //   },
-// //   //   // mode: "cors",
-// //   // })
-// //   //   .then((response) => response.json())
-// //   //   .then((data) => {
-// //   //     if (data.data && data.data.length > 0) {
-// //   //       const username = data.data[0].login; // Twitch username
-// //   //       console.log("Fetched username:", username);
-// //   //       // Send the username to your backend to create the user account
-// //   //       createUserAccount(username);
-// //   //     } else {
-// //   //       console.error("Error fetching Twitch user data:", data);
-// //   //     }
-// //   //   })
-// //   //   .catch((err) => {
-// //   //     console.error("Error fetching user info:", err);
-// //   //   });
-// // });
+window.Twitch.ext.onAuthorized(function () {
+  console.log("onAuthorized");
+  return;
+  // Turns out you can't get the user's username from the Twitch API using a user JWT token
+  // You need an access token, but I don't want to make the user deal with oauth
+  // So I'll just allow them to type their names in
+  // console.log(auth);
+  // const oauthToken = auth.helixToken;
+  // console.log(oauthToken);
+  // const clientId = auth.clientId; // Replace with your Twitch client ID
+  // const userId = auth.userId;
+  // console.log(userId);
+  // fetch("https://api.twitch.tv/helix/users", {
+  //   method: "GET",
+  //   headers: {
+  //     "client-id": clientId!,
+  //     Authorization: `Extension ${oauthToken}`,
+  //   },
+  //   // mode: "cors",
+  // })
+  //   .then((response) => response.json())
+  //   .then((data) => {
+  //     if (data.data && data.data.length > 0) {
+  //       const username = data.data[0].login; // Twitch username
+  //       console.log("Fetched username:", username);
+  //       // Send the username to your backend to create the user account
+  //       createUserAccount(username);
+  //     } else {
+  //       console.error("Error fetching Twitch user data:", data);
+  //     }
+  //   })
+  //   .catch((err) => {
+  //     console.error("Error fetching user info:", err);
+  //   });
+});
 
 // // testing bypass function
 // // export function addUser() {
